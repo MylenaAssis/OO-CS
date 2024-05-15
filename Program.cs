9fb66e7ed314e41af0025c8560be005f0b54c07e
Mylena Assis <lena.assis.dev@gmail.com>
2024-05-15 16:12:48 -0300

    Added: classe banda

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
 ﻿using System.Runtime.CompilerServices;
 
+//criando album
 Album albumDoQueen = new Album();
 albumDoQueen.Nome = "A night at the opera";
 
+//criando as musicas
 Musica musica1 = new Musica();
 musica1.Nome = "Love of my live";
 musica1.Duracao = 213;
@@ -11,10 +13,15 @@ Musica musica2 = new Musica();
 musica2.Nome = "Bohemian Rhapsody";
 musica2.Duracao = 354;
 
+//inserindo musicas no album
 albumDoQueen.AdicionarMusica(musica1);
 albumDoQueen.AdicionarMusica(musica2);
 
-albumDoQueen.ExibirMusicasDoAlbum();
+//criando banda
+Banda queen = new Banda();
+queen.Nome = "Queen";
+queen.AdicionarAlbum(albumDoQueen);
+queen.ExibirDiscografia();
 
 
 
